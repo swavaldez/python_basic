8ac0aaf7978529e529d77b5e61aacf60e304a80a
Sherwin Valdez <swavaldez@gmail.com>
2017-12-10 16:00:57 +0800

    Yields implementation

---
+++ 02_function_files/06_yields.py
@@ -0,0 +1,18 @@
+students = []
+
+
+def read_file():
+    try:
+        f = open("students.txt", "r")
+        for student in read_students(f):
+            students.append(student)
+        f.close()
+    except Exception:
+        print("Count not read the file")
+
+def read_students(f):
+    for line in f:
+        yield line
+
+read_file()
+print(students)
