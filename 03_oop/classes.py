7647b10cf1cdef5229994a5d419b526e3191c16d
Sherwin Valdez <swavaldez@gmail.com>
2017-12-10 16:19:41 +0800

    constructor

--- 03_oop/classes.py
+++ 03_oop/classes.py
@@ -1,11 +1,12 @@
 students = []
 
 class Student:
-    def add_student(self, name, student_id=332):
+    def __init__(self, name, student_id=332):
         student = {"name": name, "student_id": student_id }
         students.append(student)
+    
+    def __str__(self):
+        return "Student"
 
-student = Student()
-student.add_student("Mark")
-
-print(students)
+mark = Student("Mark")
+print(mark)
